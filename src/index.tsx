9a3f62030e6321d9db3e90b81afc8c2773395ad1
phidznhatt2 <s2bongma2s@gmail.com>
2021-02-05 14:12:14 +0700

    add layout login

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './layouts/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./layouts/App";
+import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
-import 'antd/dist/antd.css';
-import './styles/main.scss';
-import rootReducer from "./reducers"
-
+import "normalize.css";
+import "antd/dist/antd.css";
+import "./styles/main.scss";
+import rootReducer from "./reducers";
+import Login from "./layouts/Login";
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={App} />
+        <Route path="/" exact component={App} />
+        <Route path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
